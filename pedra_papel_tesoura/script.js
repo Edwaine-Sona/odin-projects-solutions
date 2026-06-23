70bc9efa2f8ec2af28e31015944c17456ac1073b
Edwaine-Sona <edwainesona@gmail.com>
2026-06-23 19:44:15 +0100

    feat: randomly select 'rock', 'paper' or 'scissors' for computer choice

--- pedra_papel_tesoura/script.js
+++ pedra_papel_tesoura/script.js
@@ -1,6 +1,15 @@
 // Computer Logic
 
-const getComputerChoice = () =>{
-    return Math.round(Math.random() * (3 - 1) + 1)
+const getComputerChoice = (choice) =>{
+    choice = Math.round(Math.random() * (3 - 1) + 1)
+    if (choice === 1) {
+        return choice = "pedra"
+    }
+    else if (choice === 2) {
+        return choice = "tesoura"
+    }
+    else{
+        return choice = "papel"
+    }
 }
 console.log(getComputerChoice())
