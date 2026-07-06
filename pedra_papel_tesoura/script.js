1205f2bdfe2a5849cae39181866abaf6ae5f7bc5
Edwaine-Sona <edwainesona@gmail.com>
2026-07-06 19:09:55 +0100

    feat: add logic only for one game round and register winner's score

--- pedra_papel_tesoura/script.js
+++ pedra_papel_tesoura/script.js
@@ -20,3 +20,54 @@ const getComputerChoice = (choice) =>{
         return choice = "papel"
     }
 }
+
+//Playround
+
+let humanScore = 0
+let computerScore = 0
+
+const playround = (humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) =>{
+
+    if (humanChoice === computerChoice) {
+        return alert("Empate!")
+    } else {
+        if (humanChoice === "pedra") {
+            if (computerChoice === "papel") {
+                computerScore++
+                return alert("Você venceu!")
+            } else {
+                humanScore++
+                return alert("Você venceu!")
+            }
+        } else {
+            if (humanChoice === "papel") {
+                if (computerChoice === "pedra") {
+                    humanScore++
+                    return alert("Você venceu!")
+                } else {
+                    computerScore++
+                    return alert("O Computador vence!")
+                }
+            } else {
+                if (humanChoice === "tesoura") {
+                    if (computerChoice === "papel") {
+                        humanScore++
+                        return alert("Você venceu!")
+                    } else {
+                        computerScore++
+                        return alert("O Computador vence!")
+                    }
+                }
+            }
+        }
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playround(humanSelection, computerSelection)
+
+console.log("Computador: " + computerSelection)
+console.log("Usuário: " + humanSelection)
+console.log("Pontuação: Computador= " + computerScore + " || Usuário= " + humanScore)
